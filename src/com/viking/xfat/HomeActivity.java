199703b76d047813f9412515ee1de8e3774ec58c
viking <viking_xie@163.com>
2014-09-29 11:10:48 +0800

    Decrease drawable resource size

--- src/com/viking/xfat/HomeActivity.java
+++ src/com/viking/xfat/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.viking.xfat;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,22 +20,27 @@ public class HomeActivity extends Activity {
     }
 
     private void initComponent() {
+        final Context context = HomeActivity.this;
+        boolean service_running = Utility.IsFloatServiceRunning(context);
         ToggleButton startButton = (ToggleButton)findViewById(R.id.startButton);
-        startButton.setChecked(Utility.IsFloatServiceRunning(HomeActivity.this));
-        startButton.setBackground(getResources().getDrawable(startButton.isChecked() ? R.drawable.start_button_on_t : R.drawable.start_button_off_t));
+
+        startButton.setChecked(service_running);
+        startButton.setBackground(getResources().getDrawable(service_running ? R.drawable.start_button_on : R.drawable.start_button_off));
         startButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                buttonView.setBackground(getResources().getDrawable(isChecked ? R.drawable.start_button_on_t : R.drawable.start_button_off_t));
+                buttonView.setBackground(getResources().getDrawable(isChecked ? R.drawable.start_button_on : R.drawable.start_button_off));
 
-                Utility.ToggleFloatService(HomeActivity.this);
+                Utility.ToggleFloatService(context);
             }
         });
         startButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (MotionEvent.ACTION_DOWN == event.getAction()) {
-                    v.setBackground(getResources().getDrawable(((ToggleButton) v).isChecked() ? R.drawable.start_button_on_ts : R.drawable.start_button_off_ts));
+                    ScaleDrawable bg = (ScaleDrawable)getResources().getDrawable(((ToggleButton) v).isChecked() ? R.drawable.start_button_on_s : R.drawable.start_button_off_s);
+                    bg.setLevel(1);
+                    v.setBackground(bg);
                 }
                 return false;
             }
