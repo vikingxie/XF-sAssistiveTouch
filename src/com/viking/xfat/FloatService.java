9e1e06f436f94011589eef11d48bc9c3071e40fb
viking <viking>
2014-10-01 22:24:51 +0800

    Set service as foreground

--- src/com/viking/xfat/FloatService.java
+++ src/com/viking/xfat/FloatService.java
@@ -1,10 +1,13 @@
 package com.viking.xfat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class FloatService extends Service {
+    static final int NOTIFY_ID = 8783638;
     private FloatButton fb = null;
 
     @Override
@@ -15,6 +18,9 @@ public class FloatService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Notification notification = new Notification();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        startForeground(NOTIFY_ID, notification);
     }
 
     @Override
@@ -23,11 +29,13 @@ public class FloatService extends Service {
             fb = new FloatButton(getApplicationContext());
             fb.show();
         }
+        ((NotificationManager)getSystemService(NOTIFICATION_SERVICE)).cancel(NOTIFY_ID);
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        stopForeground(true);
         if (null != fb) {
             fb.hide();
         }
