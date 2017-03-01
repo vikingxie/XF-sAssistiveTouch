391a0e13a0081f46df3f179d2abf216e485057b7
viking <viking_xie@163.com>
2017-03-01 11:13:50 +0800

    dip与px之间的转换

--- src/com/viking/xfat/Utility.java
+++ src/com/viking/xfat/Utility.java
@@ -194,4 +194,14 @@ public class Utility {
 
         return false;
     }
+
+    public static int dip2px(Context context, double dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, double pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
